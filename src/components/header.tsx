f22202cffbc26a6770614345a61e951c8b2f92b5
Subin <subinbs4a@gmail.com>
2024-12-19 06:59:48 +0300

    Search page changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,8 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import HeaderAuth from "./header-auth";
+import SearchInput from "./search-input";
+import { Suspense } from "react";
 
 export default function Header() {
   return (
@@ -18,7 +20,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <Input />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
